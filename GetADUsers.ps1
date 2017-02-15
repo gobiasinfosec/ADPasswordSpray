8e80fbab2a19470dd5b6e97d2d0af5782fe2249d
Tobias Funke <thedavetheory@yahoo.com>
2017-02-15 10:20:04 -0500

    Update GetADUsers.ps1

--- GetADUsers.ps1
+++ GetADUsers.ps1
@@ -1,4 +1,4 @@
 Import-Module ActiveDirectory
 
-#This will output file to c:\temp\AD_users.csv
-Get-ADUser -filter * | Select-Object SamAccountName | Export-CSV -NoTypeInformation c:\temp\AD_users.csv
+#This will output file to c:\temp\AD_users.csv, if searching for specific OU change it in the Where-Object pipe
+Get-ADUser -filter {Enabled -eq $true} | Where-Object{$_.DistinguishedName -like "*OU=*" } | Select-Object SamAccountName | Export-CSV -NoTypeInformation c:\temp\AD_users.csv
